b59cf44d5cbc9ef9ddb7a6cb9ae8799da6f75df9
Brett Seifried <seifriedbrett@gmail.com>
2022-02-23 17:45:45 -0800

    folders set up, plan in Readme, and App.js in process

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import { Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header/Header';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Profile from './Views/Profile/Profile';
+import Home from './Views/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <PrivateRoute>
+          <Profile />
+        </PrivateRoute>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
+
+export default App;
---
+++ src/components/Header/AuthBtn.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AuthBtn() {
+  return <div>AuthBtn</div>;
+}

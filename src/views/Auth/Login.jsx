e682d66d998ed80342040ee983eee2aeb9124973
Brett Seifried <seifriedbrett@gmail.com>
2022-02-24 10:16:52 -0800

    login and register forms shell created

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Sign in!</h1>
+      <form>
+        <div>
+          <input type="email" placeholder="email" />
+        </div>
+        <div>
+          <input type="password" placeholder="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <p>Need an account?</p>
+        <Link to="/register">Sign Up</Link>
+      </div>
+    </div>
+  );
 }
